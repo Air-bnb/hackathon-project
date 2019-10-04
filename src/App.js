41a67a87fbe4d127d647d0113c077991e3769bb2
Christian Roque <croqu008@fiu.edu>
2019-10-04 09:57:58 -0400

    changed text

--- src/App.js
+++ src/App.js
@@ -10,22 +10,9 @@ function App() {
 				<p>
 					Edit <code>src/App.js</code> and save to reload.
 				</p>
-				Team name!!!!
+
 				<a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-					Learn React
-				</a>
-			</header>
-		</div>
-	);
-	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-					Learn React
+					Learn React Today.
 				</a>
 			</header>
 		</div>
