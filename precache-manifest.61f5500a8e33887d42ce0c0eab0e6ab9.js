9ac9697a20a2c618f1673e91f87ed2dcf8e2219b
Christian Roque <croqu008@fiu.edu>
2019-10-04 09:42:29 -0400

    Updates

---
+++ precache-manifest.61f5500a8e33887d42ce0c0eab0e6ab9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2b63d2f1e9c37832668e62e064f1ead",
+    "url": "/hackathon-project/index.html"
+  },
+  {
+    "revision": "0b1c776896811a039fad",
+    "url": "/hackathon-project/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "23befe4542cadfd5747a",
+    "url": "/hackathon-project/static/js/2.ba1a6784.chunk.js"
+  },
+  {
+    "revision": "0b1c776896811a039fad",
+    "url": "/hackathon-project/static/js/main.fd94cc08.chunk.js"
+  },
+  {
+    "revision": "ce8587528d958d466549",
+    "url": "/hackathon-project/static/js/runtime-main.d6b61f4c.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/hackathon-project/static/media/logo.25bf045c.svg"
+  }
+]);
